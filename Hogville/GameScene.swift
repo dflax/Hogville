77be876cdaf5695aba60388e45c1cf04330a387c
Daniel Flax <dflax@TopHat.local>
2015-02-08 10:38:16 -0500

    Added background and pig.

--- Hogville/GameScene.swift
+++ Hogville/GameScene.swift
@@ -9,14 +9,30 @@
 import SpriteKit
 
 class GameScene: SKScene {
-  
-  
-  override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-    /* Called when a touch begins */
-    
-  }
-  
-  override func update(currentTime: CFTimeInterval) {
-    /* Called before each frame is rendered */
-  }
+
+	override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+		// Called when a touch begins
+	}
+
+	override func update(currentTime: CFTimeInterval) {
+		/* Called before each frame is rendered */
+	}
+
+	override init(size: CGSize) {
+		super.init(size: size)
+
+		let bg = SKSpriteNode(imageNamed: "bg_2_grassy")
+		bg.anchorPoint = CGPoint(x: 0, y: 0)
+		addChild(bg)
+
+		let pig = SKSpriteNode(imageNamed: "pig_1")
+		pig.position = CGPoint(x: size.width / 2, y: size.height / 2)
+		addChild(pig)
+	}
+
+	required init(coder aDecoder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+
+
 }
