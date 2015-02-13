997b1d9d51bde495a3fc43adfe3b13ce499e1a42
Daniel Flax <dflax@yahoo.com>
2015-02-13 09:25:53 -0500

    Added background and pig dropper

--- Hogville/GameScene.swift
+++ Hogville/GameScene.swift
@@ -8,11 +8,19 @@
 
 import SpriteKit
 
+enum ColliderType: UInt32 {
+	case Animal = 1
+	case Food = 2
+}
+
 class GameScene: SKScene {
 
 	var movingPig: Pig?
 	var lastUpdateTime: NSTimeInterval = 0.0
 	var dt: NSTimeInterval = 0.0
+
+	var homeNode = SKNode()
+	var currentSpawnTime: NSTimeInterval = 5.0
 
 	// Touch handling
 	override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
@@ -51,23 +59,65 @@ class GameScene: SKScene {
 
 	}
 
-
 	override init(size: CGSize) {
 		super.init(size: size)
 
-		let bg = SKSpriteNode(imageNamed: "bg_2_grassy")
-		bg.anchorPoint = CGPoint(x: 0, y: 0)
-		addChild(bg)
+		// Load the background and base scene elements
+		loadLevel()
 
-		let pig = Pig(imageNamed: "pig_1")
-		pig.name = "pig"
-
-		pig.position = CGPoint(x: size.width / 2, y: size.height / 2)
-		addChild(pig)
+		// Set up the pigs to load - first and forever
+		spawnAnimal()
 	}
 
 	required init(coder aDecoder: NSCoder) {
 		fatalError("init(coder:) has not been implemented")
+	}
+
+	func loadLevel () {
+		//1
+		let bg = SKSpriteNode(imageNamed:"bg_2_grassy")
+		bg.anchorPoint = CGPoint(x: 0, y: 0)
+		addChild(bg)
+
+		//2
+		let foodNode = SKSpriteNode(imageNamed:"trough_3_full")
+		foodNode.name = "food"
+		foodNode.position = CGPoint(x:250, y:200)
+		foodNode.zPosition = 1
+
+		// More code later
+
+		addChild(foodNode)
+
+		//3
+		homeNode = SKSpriteNode(imageNamed: "barn")
+		homeNode.name = "home"
+		homeNode.position = CGPoint(x: 500, y: 20)
+		homeNode.zPosition = 1
+		addChild(homeNode)
+
+		currentSpawnTime = 5.0
+	}
+
+	func spawnAnimal() {
+		//1
+		currentSpawnTime -= 0.2
+
+		//2
+		if currentSpawnTime < 1.0 {
+			currentSpawnTime = 1.0
+		}
+
+		let pig = Pig(imageNamed: "pig_1")
+		pig.position = CGPoint(x: 20, y: Int(arc4random_uniform(300)))
+		pig.name = "pig"
+
+		addChild(pig)
+
+		runAction(SKAction.sequence([SKAction.waitForDuration(currentSpawnTime), SKAction.runBlock({
+			self.spawnAnimal()
+			}
+			)]))
 	}
 
 	// Draw the lines
